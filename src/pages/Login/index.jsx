3c9cce7657c3a9ccc498f5c735ef459d4f4b629f
TineoChris <christophertineo@gmail.com>
2022-12-27 10:28:17 -0400

    create use socket context

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,93 +1,92 @@
-import React, { useState, useContext } from "react";
-import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContext";
-import Error from "../components/Error";
-import UsersSocket from "../../socket/users";
+import React, { useState, useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
+import Error from '../components/Error'
 
 const Login = () => {
-	const [error, setError] = useState("");
-	const { register, handleSubmit } = useForm();
+    const [error, setError] = useState('')
+    const { register, handleSubmit } = useForm()
 
-	// Auth Context
-	const { setCurrentUser } = useContext(AuthContext);
+    // Auth Context
+    const { setCurrentUser } = useContext(AuthContext)
 
-	// React Router V6
-	const navigate = useNavigate();
+    // React Router V6
+    const navigate = useNavigate()
 
-	const onSubmit = async (data) => {
-		const URL = "http://localhost:3000/auth/login";
+    const onSubmit = async (data) => {
+        const URL = 'http://localhost:3000/auth/login'
 
-		const options = {
-			method: "POST",
-			credentials: "include",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(data),
-		};
+        const options = {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }
 
-		try {
-			const response = await fetch(URL, options);
+        try {
+            const response = await fetch(URL, options)
 
-			if (response.ok) {
-				const result = await response.json(); //Read User info
+            if (response.ok) {
+                const result = await response.json() //Read User info
 
-				setCurrentUser(result); // Share user info in AuthContext
-				setError("");
-				navigate("/");
-			} else if (response.status === 403) {
-				setError("Incorrect password");
-			} else {
-				setError("User not found");
-			}
-		} catch (error) {
-			console.error(error);
-		}
-	};
+                setCurrentUser(result) // Share user info in AuthContext
+                setError('')
+                navigate('/')
+            } else if (response.status === 403) {
+                setError('Incorrect password')
+            } else {
+                setError('User not found')
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
-	return (
-		<main className='h-screen flex items-center justify-center bg-gray-100'>
-			<form
-				className='flex flex-col bg-gray-200 w-5/6 rounded p-2'
-				onSubmit={handleSubmit(onSubmit)}
-			>
-				<h1 className='text-2xl font-bold mx-auto mb-4'>Sign In</h1>
+    return (
+        <main className="h-screen flex items-center justify-center bg-gray-100">
+            <form
+                className="flex flex-col bg-gray-200 w-5/6 rounded p-2"
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <h1 className="text-2xl font-bold mx-auto mb-4">Sign In</h1>
 
-				<div className='mx-2'>
-					<input
-						className='w-full mb-4 p-2'
-						type='text'
-						placeholder='Type your document...'
-						{...register("document")}
-						required
-					/>
+                <div className="mx-2">
+                    <input
+                        className="w-full mb-4 p-2"
+                        type="text"
+                        placeholder="Type your document..."
+                        {...register('document')}
+                        required
+                    />
 
-					<input
-						className='w-full mb-4 p-2'
-						type='password'
-						placeholder='Type your password...'
-						{...register("password")}
-						required
-					/>
+                    <input
+                        className="w-full mb-4 p-2"
+                        type="password"
+                        placeholder="Type your password..."
+                        {...register('password')}
+                        required
+                    />
 
-					{error && <Error text={error} />}
+                    {error && <Error text={error} />}
 
-					<div className='text-md'>
-						<Link className='text-blue-800' to='/register' replace>
-							Not already registered?
-						</Link>
-					</div>
-					<button
-						className='bg-green-500 text-white font-bold text-xl w-full my-2 p-2 rounded-sm'
-						type='submit'
-					>
-						Login
-					</button>
-				</div>
-			</form>
-		</main>
-	);
-};
+                    <div className="text-md">
+                        <Link className="text-blue-800" to="/register" replace>
+                            Not already registered?
+                        </Link>
+                    </div>
+                    <button
+                        className="bg-green-500 text-white font-bold text-xl w-full my-2 p-2 rounded-sm"
+                        type="submit"
+                    >
+                        Login
+                    </button>
+                </div>
+            </form>
+        </main>
+    )
+}
 
-export default Login;
+export default Login
