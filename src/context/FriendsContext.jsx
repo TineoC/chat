7e1d9b2d3bb4bfee8e07b61a0c7135c6156c4390
TineoChris <christophertineo@gmail.com>
2022-12-27 10:28:37 -0400

    modify imports

--- src/context/FriendsContext.jsx
+++ src/context/FriendsContext.jsx
@@ -1,14 +1,13 @@
-import React, { createContext } from "react";
-import { useState } from "react";
+import React, { useState, createContext } from 'react'
 
-export const FriendsContext = createContext();
+export const FriendsContext = createContext()
 
 export const FriendsProvider = ({ children }) => {
-	const [friends, setFriends] = useState([]);
+    const [friends, setFriends] = useState([])
 
-	return (
-		<FriendsContext.Provider value={{ friends, setFriends }}>
-			{children}
-		</FriendsContext.Provider>
-	);
-};
+    return (
+        <FriendsContext.Provider value={{ friends, setFriends }}>
+            {children}
+        </FriendsContext.Provider>
+    )
+}
