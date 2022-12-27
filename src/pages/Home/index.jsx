ef93814ea1a48634c0b65ca903703cf6d98b201d
TineoChris <christophertineo@gmail.com>
2022-12-27 10:29:02 -0400

    add messages provider

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,10 +5,13 @@ import { AddContactsProvider } from '../../context/AddContactsContext'
 
 import { ChatProvider } from '../../context/ChatContext'
 import { FriendsProvider } from '../../context/FriendsContext'
+import { MessagesProvider } from '../../context/MessagesContext'
+
 import Chat from './Chat'
 
 const Home = () => {
     const [searchFriend, setSearchFriend] = useState('')
+
     return (
         <main className="my-4 w-[90%] mx-auto flex flex-col gap-y-4">
             <AddContactsProvider>
@@ -27,7 +30,7 @@ const Home = () => {
                                 placeholder="Search your chats..."
                             />
                             <Chat />
-                            <Chats search={searchFriend} />{' '}
+                            <Chats search={searchFriend} />
                         </MessagesProvider>
                     </FriendsProvider>
                 </ChatProvider>
