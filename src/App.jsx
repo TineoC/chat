97265d6e036566b7a6c8aa5773c57a21dcfa15f1
TineoChris <christophertineo@gmail.com>
2022-12-30 10:49:20 -0400

    add production and development variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Error from "./pages/Error";
-import ProtectedRoute from "./pages/ProtectedRoute";
-import { AuthProvider } from "./context/AuthContext";
+import React from 'react'
+import { Routes, Route, HashRouter } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Error from './pages/Error'
+import ProtectedRoute from './pages/ProtectedRoute'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
-	return (
-		<AuthProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path='/'
-						element={
-							<ProtectedRoute>
-								<Home />
-							</ProtectedRoute>
-						}
-					/>
-					<Route path='/login' element={<Login />} />
-					<Route path='/register' element={<Register />} />
-					<Route path='*' element={<Error />} />
-				</Routes>
-			</BrowserRouter>{" "}
-		</AuthProvider>
-	);
+    return (
+        <AuthProvider>
+            <HashRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <ProtectedRoute>
+                                <Home />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+            </HashRouter>
+        </AuthProvider>
+    )
 }
 
-export default App;
+export default App
--- src/socket/config.js
+++ src/socket/config.js
@@ -1,4 +1,4 @@
-const SERVER = 'http://localhost:80'
+const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER_URL
 
-export const USERS_URL = `${SERVER}/users`
-export const MESSAGES_URL = `${SERVER}/messages`
+export const USERS_URL = `${SOCKET_SERVER_URL}/users`
+export const MESSAGES_URL = `${SOCKET_SERVER_URL}/messages`
