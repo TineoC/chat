d5afcec19e6914c7950fbc6324516bb46935c1ae
TineoC <christophertineo02@gmail.com>
2023-01-04 13:12:00 -0400

    remove messages socket

--- src/socket/config.js
+++ src/socket/config.js
@@ -2,4 +2,3 @@ const SOCKET_SERVER_URL =
 	import.meta.env.VITE_SOCKET_SERVER_URL || `http://localhost:99`;
 
 export const USERS_URL = `${SOCKET_SERVER_URL}/users`;
-export const MESSAGES_URL = `${SOCKET_SERVER_URL}/messages`;
