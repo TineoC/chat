642c598f9f1d6118690d50b10f316f220648125b
TineoC <christophertineo02@gmail.com>
2023-01-05 09:42:54 -0400

    log users url

--- src/socket/users.js
+++ src/socket/users.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { USERS_URL } from "./config";
 
-console.log(import.meta.env.VITE_SOCKET_SERVER_URL);
+console.log(USERS_URL);
 
 const UsersSocket = io(USERS_URL, {
 	autoConnect: false,
