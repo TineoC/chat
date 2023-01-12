07e957f5624632e4768bd4f327e810a52433456f
TineoC <christophertineo02@gmail.com>
2023-01-12 11:24:04 -0400

    add time to messages

--- src/pages/Home/Chat/Message.jsx
+++ src/pages/Home/Chat/Message.jsx
@@ -7,24 +7,39 @@ const Message = ({ message }) => {
 
   const sentByFriend = message.senderId === currentUser.uid;
 
-  if (sentByFriend) return <SelfMessage content={message.message} />;
+  if (sentByFriend) return <SelfMessage message={message} />;
 
-  return <ReceivedMessage content={message.message} />;
+  return <ReceivedMessage message={message} />;
 };
 
 const commonStyles =
-  "w-max max-w-[60%] text-white text-xs rounded-md px-4 py-1 break-words";
+  "w-max max-w-[70%] min-w-[40%] text-white text-sm rounded-md px-4 py-1 break-words";
 
-const SelfMessage = ({ content }) => {
+const SelfMessage = ({ message }) => {
   return (
-    <div className={`${commonStyles} bg-green-500  self-end`}>{content}</div>
+    <div className={`${commonStyles} bg-green-500 self-end flex flex-col`}>
+      {message.message}
+
+      <Time seconds={message.date.seconds} />
+    </div>
   );
 };
 
-const ReceivedMessage = ({ content }) => {
+const ReceivedMessage = ({ message }) => {
   return (
-    <div className={`${commonStyles} bg-blue-500  self-start`}>{content}</div>
+    <div className={`${commonStyles} bg-blue-500 self-start flex flex-col`}>
+      {message.message}
+
+      <Time seconds={message.date.seconds} />
+    </div>
   );
+};
+
+const Time = ({ seconds }) => {
+  const date = new Date(seconds * 1000);
+
+  const time = date.toLocaleTimeString() || "";
+  return <small className="text-right text-xs">{time}</small>;
 };
 
 export default Message;
