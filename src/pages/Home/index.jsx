cd42db8b6cd2db281fe7a5253cbdc6d81b451c68
TineoC <christophertineo02@gmail.com>
2023-02-03 09:24:34 -0400

    remove unused context calls

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,35 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 
-import { AddContactsProvider } from "../../context/AddContactsContext";
 import { ChatProvider } from "../../context/ChatContext";
-import { FriendsProvider } from "../../context/FriendsContext";
 import { MessagesProvider } from "../../context/MessagesContext";
 import Chats from "./components/Chats";
 import Header from "./components/Header";
 
 const Home = () => {
   return (
-    <main className="my-6 w-[90%] mx-auto flex flex-col">
-      <AddContactsProvider>
-        <FriendsProvider>
-          <ChatProvider>
-            <MessagesProvider>
-              <div className="fixed w-[90%] top-0 z-1 pt-4 bg-white">
-                <Header />
+    <main className="my-4 w-full mx-auto flex flex-col">
+      <ChatProvider>
+        <MessagesProvider>
+          <Header />
 
-                <h1 className="text-2xl my-2 font-bold">Chats</h1>
-                <input
-                  className="bg-gray-100 w-full my-2 p-2 rounded-md"
-                  type="text"
-                  placeholder="Search your chats..."
-                />
-              </div>
-
-              <Chats />
-            </MessagesProvider>
-          </ChatProvider>
-        </FriendsProvider>
-      </AddContactsProvider>
+          <Chats />
+        </MessagesProvider>
+      </ChatProvider>
     </main>
   );
 };
