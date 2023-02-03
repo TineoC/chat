91c54726c3f1a072022343a5e12ca9006bd1cae1
TineoC <christophertineo02@gmail.com>
2023-02-03 09:25:15 -0400

    check for no chats case

--- src/pages/Home/components/Chats/index.jsx
+++ src/pages/Home/components/Chats/index.jsx
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 import React, { useContext } from "react";
 
 import { ChatContext } from "../../../../context/ChatContext";
@@ -10,7 +12,7 @@ const Chats = () => {
 
   const chats = useFetchChats();
 
-  if (!chats)
+  if (typeof chats === "undefined")
     return (
       <span className="flex flex-row text-sm text-slate-400">
         Add some contacts so you can chat with people...
@@ -22,23 +24,57 @@ const Chats = () => {
   };
 
   return (
-    <div className="py-4">
-      {chats.map((chat) => {
+    <div className="mt-[10rem] w-full mx-auto">
+      {Object.values(chats).map(({ userInfo, lastMessage }) => {
+        function formatLastMessageTime(lastMessageDate) {
+          // last message was today
+          // format hh:mm
+
+          const isToday = moment(lastMessageDate).isSame(new Date(), "day");
+
+          if (isToday) {
+            //	return time
+            return moment(lastMessageDate).format("hh:mm A");
+          }
+
+          // last message was this year
+          // format dd/mm
+
+          const isThisYear = moment(lastMessageDate).isSame(new Date(), "year");
+          if (isThisYear) {
+            return moment(lastMessageDate).format("DD/MM");
+          }
+
+          // last message was not this year
+          // format dd/mm/yyyy
+          return lastMessageDate.toLocaleDateString("en-US", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          });
+        }
+
         return (
           <div
-            key={chat[0]}
-            className="py-2 rounded-md hover:bg-gray-100 mx-auto cursor-pointer overflow-auto"
+            key={userInfo.uid}
+            className="py-2 rounded-md hover:bg-gray-100 cursor-pointer overflow-auto"
             onClick={() => {
-              handleSelect(chat[1].userInfo);
+              handleSelect(userInfo);
             }}
           >
-            <div className="flex flex-row gap-4">
-              <Avatar url={chat[1].userInfo.photoURL} />
+            <div className="flex flex-row gap-4 w-[90%] mx-auto">
+              <Avatar url={userInfo.photoURL} />
 
               <div className="flex flex-col w-3/4">
-                <h3 className="text-xl">{chat[1].userInfo.displayName}</h3>
+                <div className="flex flex-row items-center justify-between">
+                  <h3 className="text-xl">{userInfo.displayName}</h3>
+                  <span className="font-medium text-sm text-slate-400">
+                    {lastMessage.date &&
+                      formatLastMessageTime(lastMessage.date.toDate())}
+                  </span>
+                </div>
                 <span className="font-normal text-md text-slate-500 truncate">
-                  {chat[1].lastMessage?.message}
+                  {lastMessage?.message}
                 </span>
               </div>
             </div>
